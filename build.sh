c181cd0bc9fa67187fc3e021f807dca4d5d948af
ggranum <geoff.granum@gmail.com>
2016-09-02 11:32:30 +0100

    Add readme, and may as well keep the files in git.

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-npm logout
+# npm logout
 npm install
 cp bower_components/asciidoctor.js/README.adoc ./redist/ASCIIDOCTOR_README.adoc
+cp -R bower_components/asciidoctor.js/dist/ ./redist/
 cd redist
-npm login
+# npm login
 npm publish
-rm ./redist/ASCIIDOCTOR_README.adoc
